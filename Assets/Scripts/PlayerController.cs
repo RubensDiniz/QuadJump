52457a2ea3432b05e0fb63b8049af9463b814253
Rubens Diniz <rubensadiniz@edu.unifor.br>
2020-04-28 16:16:13 -0300

    State machine (broken)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,69 +8,160 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D rb;
 
     public LayerMask groundLayer;
-    public float radius = 0.1f;
     public Transform leftFoot;
     public Transform rightFoot;
+    private int shouldCheckGrounded;
+    private float radius = 0.01f;
     public bool isGrounded => Physics2D.OverlapCircle(leftFoot.position, radius, groundLayer) ||
                               Physics2D.OverlapCircle(rightFoot.position, radius, groundLayer);
 
-    public float walkingSpeed = 8f;
+    public float walkingSpeed;
     float hinput;
 
-    public float jumpingForce = 20f;
+    public float jumpingForce;
     int jumpCount;
+
+    public Coroutine DashCoroutine;
+    public float dashTime;
+    public float dashForce;
+
+    private float originalGravity;
+
+    [Serializable]
+    public enum PlayerState {
+        Normal,
+        Dashing,
+        FallingSlowly,
+        Paused
+    }
+
+    [SerializeField] private PlayerState _state;
+
+    public PlayerState State {
+        get => _state;
+
+        set {
+
+            Debug.Log(value);
+            switch (value){
+                case PlayerState.Normal:
+                    rb.gravityScale = originalGravity;
+                    break;
+                case PlayerState.Dashing:
+                    rb.gravityScale = 0;
+                    break;
+                case PlayerState.FallingSlowly:
+                    break;
+                case PlayerState.Paused:
+                    break;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
-        jumpCount = 0;
-    }
 
-    void FixedUpdate() {
-        CheckGrounded();
+        jumpCount = 0;
+        shouldCheckGrounded = 0;
+
+        originalGravity = rb.gravityScale;
+        State = PlayerState.Normal;
     }
 
     // Update is called once per frame
     void Update() {
-        Move();
-        Jump();
+        switch (State) {
+            case PlayerState.Normal:
+                MoveLogic();
+                JumpLogic();
+                break;
+            case PlayerState.Dashing:
+                JumpLogic();
+                break;
+            case PlayerState.FallingSlowly:
+                break;
+            case PlayerState.Paused:
+                break;
+        }
+
+       // MoveLogic();
+       // JumpLogic();
     }
 
-    private void Move() {
-        hinput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(hinput * walkingSpeed, rb.velocity.y);
-        
-        //print(rb.velocity.x);
+    void FixedUpdate() {
+        GroundedLogic();
+
     }
 
-    private void CheckGrounded() {
-        //isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.4f, transform.position.y - 0.49f),
-                                           //new Vector2(transform.position.x + 0.4f, transform.position.y - 0.49f), groundLayer);
-
-        if (isGrounded) {
-            jumpCount = 0;
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (DashCoroutine != null) {
+            StopCoroutine(DashCoroutine);
+            EndDash();
         }
     }
 
-    private void Jump() {
-        
+    private void MoveLogic() {
+        hinput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(hinput * walkingSpeed, rb.velocity.y);
+        //print(rb.velocity.x);
+    }
+
+    private void JumpLogic() {
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 5) {
+            shouldCheckGrounded = 3;
+
             switch (jumpCount) {
                 case 0:
                 case 1:
-                    rb.velocity = new Vector2(rb.velocity.x, jumpingForce);
-                    jumpCount++;
+                    Jump();
                     break;
                 case 2:
-                    jumpCount++;
+                    Dash();
                     break;
                 case 3:
-                    jumpCount++;
+                    Shoot();
                     break;
             }
 
-            Debug.Log(jumpCount);
+            jumpCount++;
+
+            //Debug.Log(jumpCount);
+        }
+    }
+    
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpingForce);
+    }
+
+    private void Dash() {
+        DashCoroutine = StartCoroutine(DashLogic());
+    }
+
+    private IEnumerator DashLogic() {
+        rb.velocity = new Vector2(dashForce, 0);
+        State = PlayerState.Dashing;
+        yield return new WaitForSeconds(dashTime);
+        EndDash();
+    }
+
+    private void EndDash() {
+        DashCoroutine = null;
+        rb.gravityScale = originalGravity;
+        //rb.velocity = new Vector2(0, 0);
+        State = PlayerState.Normal;
+    }
+
+    private void Shoot() {
+
+    }
+
+    private void GroundedLogic() {
+        if (shouldCheckGrounded <= 0 && isGrounded) {
+            jumpCount = 0;
+        } else if (shouldCheckGrounded > -5) {
+            shouldCheckGrounded--;
         }
     }
 }
